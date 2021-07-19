b6f34b48a6e8f219cfd505247cbd95d89159201f
Chen Zhenge <Mr.ChenWithCapsule@outlook.com>
2021-07-19 22:40:23 +0800

    Add MyVideosActivity

---
+++ app/src/main/java/com/example/faketok/MyVideosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.faketok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyVideosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_videos)
+    }
+}
