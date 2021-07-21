d3c5d8872561aa9986856d44b35b6056d3476289
191250129 <191250129@smail.nju.edu.cn>
2021-07-21 16:16:19 +0800

    录制功能

--- app/src/main/java/com/example/faketok/CaptureActivity.kt
+++ app/src/main/java/com/example/faketok/CaptureActivity.kt
@@ -1,13 +1,39 @@
 package com.example.faketok
 
 import android.os.Bundle
+import android.view.SurfaceView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.faketok.util.*;
 
 class CaptureActivity : AppCompatActivity() {
+
+    private lateinit var video: SurfaceView
+    private lateinit var controler: Button
+    private lateinit var videoRecorder: VideoRecorder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_capture)
+
+        video=findViewById(R.id.videoRecorder)
+        controler=findViewById(R.id.VideoController)
+        videoRecorder= VideoRecorder()
+        controler.setOnClickListener{startRecording()}
+    }
+
+    private fun startRecording() {
+        if (!videoRecorder?.isRecording){
+            videoRecorder?.startRecording(video,this)
+            controler.setText(R.string.endVideo)
+        }else{
+            videoRecorder?.stopRecording()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        videoRecorder?.stopRecording()
     }
 
 }
